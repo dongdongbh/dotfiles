50389049352142f59bb0ef2c4687dba1323a0741
dongdongbh <18310682633@163.com>
2022-10-28 14:28:51 +0800

    add bspwm and sxhkd settings, other stuffs like polybar rofi betterlockscreen fonts

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# Launch bar
+echo "---" | tee -a /tmp/mybar.log 
+polybar mybar 2>&1 | tee -a /tmp/mybar.log & disown
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload example &
-done
-
-echo "Polybar launched..."
+echo "Bars launched..."
