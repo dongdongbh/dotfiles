493e1ceaa2f33ac1a0092e7d599954ead61063c9
dongdongbh <18310682633@163.com>
2025-09-06 16:20:30 -0400

    hyprland setup and install script

--- install.sh
+++ install.sh
@@ -1,10 +1,38 @@
 #!/bin/bash
-sudo apt update
-sudo apt-get install zsh stow ranger bspwm feh rofi fd-find ripgrep zathura maim gnome-session-canberra xclip thefuck suckless-tools dunst autorandr imagemagick xss-lock 
+pacman -Syu
+pacman -S sudo 
+sudo pacman -S vim 
+sudo pacman -S mesa vulkan-intel libva-intel-driver
+sudo pacman -S pipewire pipewire-pulse wireplumber
+sudo pacman -S hyprland xorg-xwayland alacritty rofi waybar mako thunar hyprpaper hyprlock hypridle swaybg xdg-desktop-portal-hyprland hypridle wl-clipboard cliphist grim slurp
+sudo pacman -S zsh git stow zsthura ripgrep feh rofi neovim
+sudo pacman -S openssh nebula udisks2 udiskie git-delta fd yazi duf dust dunst flameshot
+
+sudo pacman -S sddm
+sudo systemctl enable sddm
+sudo pacman -S noto-fonts noto-fonts-emoji ttf-jetbrains-mono-nerd
+
+
+
+sudo pacman -S --needed base-devel git
+
+# Clone and build yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+
+yay -S google-chrome
+
+
+
+chsh -s $(which zsh)
 sudo chsh -s $(which zsh)
-mkdir $HOME/Pictures/Screenshots/
+stow hypr waybar
+mkdir -p $HOME/Pictures/Screenshots/
 rm ~/.zshrc
-stow tmux_cfg zsh
+stow tmux_cfg zsh fonts git rofi vim scripts
 
 ln -s -f ~/.config/tmux_cfg/tmux.conf ~/.tmux.conf
 ln -s ~/.config/tmux_cfg/tmux.conf.local ~/.tmux.conf.local
@@ -12,3 +40,5 @@ ln -s ~/.config/tmux_cfg/tmux.conf.local ~/.tmux.conf.local
 # We use Alacritty's default Linux config directory as our storage location here.
 mkdir -p ~/.config/alacritty/themes
 git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
+
+stow alacritty
