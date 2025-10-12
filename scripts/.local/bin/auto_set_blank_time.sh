af95b9f3913552167dd8b228abfdda20e4ef4c10
dongdongbh <18310682633@163.com>
2025-10-12 14:42:15 -0400

    fix poweroff for waybar

--- scripts/.local/bin/auto_set_blank_time.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if acpi -a | grep -q 'off-line'; then
-    xset dpms 90 120 180 # Set screen blank time to 2 minutes when on battery
-else
-    xset dpms 300 600 1200 # Set screen blank time to 5 minutes when charging
-fi
-
